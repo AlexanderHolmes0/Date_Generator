366989e2cef694c956ac87c96a8e76c304b21fe7
AlexanderHolmes0 <aholme27@vols.utk.edu>
2023-06-08 10:33:30 -0400

    Main Stuffs

---
+++ Rando_Date_Ideas/app.R
@@ -0,0 +1,52 @@
+library(shiny)
+library(shinyWidgets)
+library(tidyverse)
+library(shinythemes)
+
+ideas <- read.csv("Ideas.csv")
+ideas <- ideas[-1, ]
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  theme = shinytheme("superhero"),
+  tags$link(
+    rel = "shortcut icon", href = "favicon.ico"
+  ),
+
+  # Application title
+  column(
+    width = 4, align = "center", offset = 4,
+    br(),
+    br(),
+    titlePanel("Date Night Generator"),
+    br(),
+    img(src = "datenight.gif"),
+    br(),
+    br(),
+    actionBttn(
+      inputId = "generate",
+      label = "Get Idea",
+      icon = icon(name = "calendar"),
+      style = "unite",
+      color = "royal"
+    ),
+    br(),
+    br(),
+    span(textOutput(outputId = "idea"), 
+         style = "font-size:20px; font-family:arial; font-style:italic; color:white")
+  )
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  output$idea <- eventReactive(input$generate,
+    {
+      sample(ideas, 1, replace = TRUE)
+    },
+    ignoreInit = TRUE
+  )
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
